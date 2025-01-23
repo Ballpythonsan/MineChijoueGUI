7ddac09d60aa861b782f7f768a7498004587a8d8
Ballpythonsan <ballpythonsan@gmail.com>
2025-01-23 20:17:46 +0900

    インプット画像と地図絵プレビューの追加

--- src/ballpythonsan/minechijouegui/MineChijoueGUI.java
+++ src/ballpythonsan/minechijouegui/MineChijoueGUI.java
@@ -3,6 +3,7 @@ package ballpythonsan.minechijouegui;
 import java.awt.Color;
 import java.awt.EventQueue;
 import java.awt.Font;
+import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.BufferedReader;
@@ -13,10 +14,12 @@ import java.util.Properties;
 
 import javax.swing.GroupLayout;
 import javax.swing.GroupLayout.Alignment;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JCheckBox;
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTabbedPane;
@@ -56,7 +59,7 @@ public class MineChijoueGUI extends JFrame {
 		setResizable(false);
 		setTitle("地上絵作成支援GUI");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 893, 467);
+		setBounds(100, 100, 893, 485);
 		JPanel contentPane = new JPanel();
 		contentPane.setBackground(Color.LIGHT_GRAY);
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
@@ -64,9 +67,7 @@ public class MineChijoueGUI extends JFrame {
 		//ここにおいておきたい---------
 
 		
-		JCheckBox chckbxNewCheckBox = new JCheckBox("代替設定を使用する");
-		chckbxNewCheckBox.setBackground(Color.WHITE);
-		chckbxNewCheckBox.setForeground(Color.BLACK);
+		JCheckBox isUseSubstitute = new JCheckBox("代替設定を使用する");
 		
 		JTable substituteTable = new JTable(new DefaultTableModel(
 				new String[][] {
@@ -141,21 +142,27 @@ public class MineChijoueGUI extends JFrame {
 				}
 			});
 		
-		//--------------------------
+		JTextArea consoleLog = new JTextArea();
+		JTabbedPane imageTab = new JTabbedPane(JTabbedPane.TOP);
+		JPanel inputImage = new JPanel();
+		JLabel inputImageLabel = new JLabel();
+		JPanel outputImagePreview = new JPanel();
+		JLabel outputImagePreviewLabel = new JLabel();
+		
+		// --------------------------
 		
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
-		JTabbedPane tabbedPane = new JTabbedPane(JTabbedPane.TOP);
-		tabbedPane.setBounds(12, 10, 384, 408);
-		contentPane.add(tabbedPane);
+		JTabbedPane settingTab = new JTabbedPane(JTabbedPane.TOP);
+		settingTab.setBounds(34, 10, 384, 408);
+		contentPane.add(settingTab);
 		
 		// 基本設定タブ
-		JPanel setting = new JPanel();
-		setting.setBackground(Color.GRAY);
-		setting.setBounds(31, 29, 365, 374);
-		// contentPane.add(setting);
-		tabbedPane.addTab("メイン設定", null, setting, "地上絵にしたい画像の選択、ログ、作成実行");
+		JPanel mainSetting = new JPanel();
+		mainSetting.setBackground(Color.GRAY);
+		mainSetting.setBounds(31, 29, 365, 374);
+		settingTab.addTab("メイン設定", null, mainSetting, "地上絵にしたい画像の選択、ログ、作成実行");
 		
 		JTextPane textPane_2 = new JTextPane();
 		textPane_2.setText("地上絵にしたい画像を選択してください");
@@ -184,6 +191,19 @@ public class MineChijoueGUI extends JFrame {
 					}
 					File file = fileChooser.getSelectedFile();
 					inputImagePath.setText(file.getPath());
+					try {
+						inputImageLabel.setIcon(resizeImageIcon(imageTab.getWidth(),imageTab.getHeight(),new ImageIcon(inputImagePath.getText())));
+					} catch(Exception ex) {
+						consoleLog.append(ex + "\n");
+					}
+					try {
+						ProcessBuilder pb = new ProcessBuilder("java", "-jar", "./chijoue.jar", inputImagePath.getText(), "--preview");
+						pb.start().waitFor();
+						String previewImagePath = file.getName().substring(0, file.getName().lastIndexOf('.')) + "_conv.png";
+						outputImagePreviewLabel.setIcon(resizeImageIcon(imageTab.getWidth(),imageTab.getHeight(),new ImageIcon(previewImagePath)));
+					} catch(Exception ex) {
+						consoleLog.append(ex + "\n");
+					}
 					return;
 					// if (file.getName().endsWith("png") || file.getName().endsWith("jpeg") || file.getName().endsWith("jpg") || file.getName().endsWith("bmp")) {
 
@@ -200,7 +220,7 @@ public class MineChijoueGUI extends JFrame {
 		
 		// ログの出力画面
 		JScrollPane scrollPane = new JScrollPane();
-		JTextArea consoleLog = new JTextArea();
+		// JTextArea consoleLog = new JTextArea();
 		consoleLog.setFont(new Font("Monospaced", Font.PLAIN, 13));
 		scrollPane.setViewportView(consoleLog);
 		consoleLog.setEditable(false);
@@ -221,7 +241,7 @@ public class MineChijoueGUI extends JFrame {
 						
 						//代替ブロック設定のボタンが押されていたら、
 						String substitutePath = "";
-						if (chckbxNewCheckBox.isSelected()) {
+						if (isUseSubstitute.isSelected()) {
 							
 							substitutePath = "substitute.properties";
 							Properties properties = new Properties();
@@ -261,34 +281,34 @@ public class MineChijoueGUI extends JFrame {
 			}
 		});
 		
-		GroupLayout gl_setting = new GroupLayout(setting);
-		gl_setting.setHorizontalGroup(
-			gl_setting.createParallelGroup(Alignment.LEADING)
-				.addGroup(gl_setting.createSequentialGroup()
+		GroupLayout gl_mainSetting = new GroupLayout(mainSetting);
+		gl_mainSetting.setHorizontalGroup(
+			gl_mainSetting.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_mainSetting.createSequentialGroup()
 					.addContainerGap()
-					.addGroup(gl_setting.createParallelGroup(Alignment.LEADING)
-						.addGroup(gl_setting.createSequentialGroup()
+					.addGroup(gl_mainSetting.createParallelGroup(Alignment.LEADING)
+						.addGroup(gl_mainSetting.createSequentialGroup()
 							.addComponent(scrollPane, GroupLayout.DEFAULT_SIZE, 355, Short.MAX_VALUE)
 							.addContainerGap())
-						.addGroup(Alignment.TRAILING, gl_setting.createSequentialGroup()
+						.addGroup(Alignment.TRAILING, gl_mainSetting.createSequentialGroup()
 							.addComponent(textPane_2, GroupLayout.DEFAULT_SIZE, 267, Short.MAX_VALUE)
 							.addGap(100))
-						.addGroup(Alignment.TRAILING, gl_setting.createSequentialGroup()
+						.addGroup(Alignment.TRAILING, gl_mainSetting.createSequentialGroup()
 							.addComponent(inputImagePath, GroupLayout.DEFAULT_SIZE, 219, Short.MAX_VALUE)
 							.addPreferredGap(ComponentPlacement.RELATED)
 							.addComponent(FileChooser, GroupLayout.PREFERRED_SIZE, 129, GroupLayout.PREFERRED_SIZE)
 							.addContainerGap())
-						.addGroup(gl_setting.createSequentialGroup()
+						.addGroup(gl_mainSetting.createSequentialGroup()
 							.addComponent(execute, GroupLayout.DEFAULT_SIZE, 355, Short.MAX_VALUE)
 							.addContainerGap())))
 		);
-		gl_setting.setVerticalGroup(
-			gl_setting.createParallelGroup(Alignment.LEADING)
-				.addGroup(gl_setting.createSequentialGroup()
+		gl_mainSetting.setVerticalGroup(
+			gl_mainSetting.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_mainSetting.createSequentialGroup()
 					.addContainerGap()
 					.addComponent(textPane_2, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
 					.addGap(8)
-					.addGroup(gl_setting.createParallelGroup(Alignment.BASELINE)
+					.addGroup(gl_mainSetting.createParallelGroup(Alignment.BASELINE)
 						.addComponent(inputImagePath, GroupLayout.PREFERRED_SIZE, 21, GroupLayout.PREFERRED_SIZE)
 						.addComponent(FileChooser, GroupLayout.PREFERRED_SIZE, 24, GroupLayout.PREFERRED_SIZE))
 					.addPreferredGap(ComponentPlacement.UNRELATED)
@@ -297,17 +317,21 @@ public class MineChijoueGUI extends JFrame {
 					.addComponent(execute, GroupLayout.DEFAULT_SIZE, 21, Short.MAX_VALUE)
 					.addGap(11))
 		);
-		setting.setLayout(gl_setting);
+		mainSetting.setLayout(gl_mainSetting);
 		
 		// 代替ブロック設定タブ
 		JPanel substitute = new JPanel();
 		substitute.setBackground(Color.GRAY);
-		tabbedPane.addTab("代替設定", null, substitute, "代替ブロックの設定");
+		settingTab.addTab("代替設定", null, substitute, "代替ブロックの設定");
 		
 		JTextPane textPane = new JTextPane();
 		textPane.setText("代替ブロックの設定");
 		textPane.setForeground(Color.WHITE);
 		textPane.setBackground(Color.DARK_GRAY);
+		
+		// JCheckBox isUseSubstitute = new JCheckBox("代替設定を使用する");
+		isUseSubstitute.setBackground(Color.WHITE);
+		isUseSubstitute.setForeground(Color.BLACK);
 		
 		JScrollPane scrollPane_1 = new JScrollPane();
 		
@@ -322,7 +346,7 @@ public class MineChijoueGUI extends JFrame {
 						.addGroup(gl_substitute.createSequentialGroup()
 							.addComponent(textPane, GroupLayout.PREFERRED_SIZE, 223, GroupLayout.PREFERRED_SIZE)
 							.addPreferredGap(ComponentPlacement.RELATED)
-							.addComponent(chckbxNewCheckBox))
+							.addComponent(isUseSubstitute))
 						.addComponent(scrollPane_1, GroupLayout.PREFERRED_SIZE, 355, GroupLayout.PREFERRED_SIZE))
 					.addContainerGap(GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
 		);
@@ -331,7 +355,7 @@ public class MineChijoueGUI extends JFrame {
 				.addGroup(gl_substitute.createSequentialGroup()
 					.addContainerGap()
 					.addGroup(gl_substitute.createParallelGroup(Alignment.LEADING, false)
-						.addComponent(chckbxNewCheckBox, 0, 0, Short.MAX_VALUE)
+						.addComponent(isUseSubstitute, 0, 0, Short.MAX_VALUE)
 						.addComponent(textPane, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
 					.addPreferredGap(ComponentPlacement.RELATED, 55, Short.MAX_VALUE)
 					.addComponent(scrollPane_1, GroupLayout.PREFERRED_SIZE, 334, GroupLayout.PREFERRED_SIZE)
@@ -339,5 +363,52 @@ public class MineChijoueGUI extends JFrame {
 		);
 		substitute.setLayout(gl_substitute);
 		
+		// JTabbedPane imageTab = new JTabbedPane(JTabbedPane.TOP);
+		imageTab.setBounds(459, 10, 384, 408);
+		contentPane.add(imageTab);
+		
+		// JPanel inputImage = new JPanel();
+		imageTab.addTab("入力画像", null, inputImage, null);
+		
+		// JLabel inputImageLabel = new JLabel();
+		inputImage.setBackground(Color.GRAY);
+		inputImage.add(inputImageLabel);
+		
+		// JPanel outputImagePreview = new JPanel();
+		imageTab.addTab("地図絵プレビュー", null, outputImagePreview, null);
+		
+		// JLabel outputImagePreviewLabel = new JLabel();
+		outputImagePreview.setBackground(Color.GRAY);
+		outputImagePreview.add(outputImagePreviewLabel);
+		
 	}
+	
+	public static ImageIcon resizeImageIcon(int panelWidth, int panelHeight, ImageIcon inputIcon) {
+        if (inputIcon == null || inputIcon.getImage() == null) {
+            throw new IllegalArgumentException("Input ImageIcon is null or invalid.");
+        }
+
+        // 元画像の幅と高さを取得
+        int imgWidth = inputIcon.getIconWidth();
+        int imgHeight = inputIcon.getIconHeight();
+
+        // アスペクト比を計算
+        double aspectRatio = (double) imgWidth / imgHeight;
+
+        // 新しい幅と高さを計算
+        int newWidth = panelWidth;
+        int newHeight = (int) (panelWidth / aspectRatio);
+
+        // 高さがパネルを超える場合は幅を調整
+        if (newHeight > panelHeight) {
+            newHeight = panelHeight;
+            newWidth = (int) (panelHeight * aspectRatio);
+        }
+
+        // 画像をリサイズ
+        Image resizedImage = inputIcon.getImage().getScaledInstance(newWidth, newHeight, Image.SCALE_SMOOTH);
+
+        // リサイズされた ImageIcon を返す
+        return new ImageIcon(resizedImage);
+    }
 }
